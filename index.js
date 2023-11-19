9d067c90cbae9a1dee6c08d134afbe59f0f19e22
omarjafor <omarjaforchy@gmail.com>
2023-11-19 10:56:55 +0600

    successfully connected to MongoDB

--- index.js
+++ index.js
@@ -2,10 +2,35 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+require('dotenv').config();
 
 // Middleware 
 app.use(cors());
 app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.21hcnfr.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("You successfully connected to MongoDB!");
+    } finally {
+        
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Boss server is running')
