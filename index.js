155b9056c4b4284b859c1fb6e32a43b0a7d0c708
omarjafor <omarjaforchy@gmail.com>
2023-11-19 18:48:04 +0600

    reviews collection successfully find

--- index.js
+++ index.js
@@ -24,9 +24,15 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         const menuCollection = client.db('bistroDB').collection('menu');
+        const reviewCollection = client.db('bistroDB').collection('reviews');
 
         app.get('/menu', async(req, res) => {
             const result = await menuCollection.find().toArray();
+            res.send(result);
+        })
+
+        app.get('/reviews', async (req, res) => {
+            const result = await reviewCollection.find().toArray();
             res.send(result);
         })
 
